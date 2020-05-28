2864f422d6b0fe1b7ae2ffb1013587bf688955db
Aditya Naik <adityan9900@gmail.com>
2020-05-27 21:12:14 -0400

    Base funtionality in trader.py implemented as functions

--- trader.py
+++ trader.py
@@ -1,13 +1,13 @@
-#initialize (daily):
-#create and train new models (one for high and low) - this will keep models up to date and reset scaling functions - this will probably also need a different way to load data?
+# initialize (daily):
+# create and train new models (one for high and low) - this will keep models up to date and reset scaling functions - this will probably also need a different way to load data?
 
-#run (throughout the day starting at )
-#need a clock around the whole thing that repeats forever but only does the thing inside if a minute has passed
-#loads data (method)
-#import and run model from modelBuilding to get predictions
-#math magic about what I want to buy and sell based on my positions (need to be stored in CSV? - probably just pandas table is good)
-#trade (method that sends commands and also stores results in some data thing)
-#print out how long this iteration took (hopefully less than a minute)
+# run (throughout the day starting at )
+# need a clock around the whole thing that repeats forever but only does the thing inside if a minute has passed
+# loads data (method)
+# import and run model from modelBuilding to get predictions
+# math magic about what I want to buy and sell based on my positions (need to be stored in CSV? - probably just pandas table is good)
+# trade (method that sends commands and also stores results in some data thing)
+# print out how long this iteration took (hopefully less than a minute)
 
 import math
 # import pandas_datareader as web
@@ -22,35 +22,40 @@ import matplotlib.pyplot as plt
 from modelBuilding import scaler
 from modelBuilding import modelBuilder
 
+def train_and_test_IPP(data_path="aapl.csv"):
 
-rawData = pd.read_csv("aapl.csv", index_col=0)
-
-rawDataNum = rawData.values
-
-trainLen = math.ceil(len(rawData) * .8)
-
-testData = rawDataNum[trainLen - 180: , :]
-xTest = []
-yTest = rawDataNum[trainLen:(len(rawDataNum) - 15), 3]
-for i in range(180, len(testData) - 15):
-    xTest.append(testData[i-180:i, :])
+    model = modelBuilder(180, 15, 3, .8, 1, data_path)
+    model.test()
+    print(model.error)
 
 
-testModel = modelBuilder(180, 15, 3, .8, 1, "aapl.csv")
+def train_and_test_PP(data_path="aapl.csv"):
 
-yPredict = testModel.test(xTest).flatten()
+    model = modelBuilder(180, 15, 3, .8, 1, data_path)
+    model.test()
+    print(model.error)
 
-training = rawData.iloc[:, 0:trainLen]
-valid = rawData[['close','Volume']][trainLen:(len(yTest) + trainLen)]
-valid.columns = ['close', 'Predictions']
 
-valid['Predictions'] = yPredict
+def graph_prediction(raw_data, train_len, y_test, y_pred):
 
-plt.figure(figsize=(100,25))
-plt.title('Model')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.plot(training['close'])
-plt.plot(valid[['close', 'Predictions']])
-plt.legend(['Train', 'Val', 'Predictions'])
-plt.show()
+    training = raw_data.iloc[:, 0:train_len]
+    valid = raw_data[['close', 'Volume']][train_len:(len(y_test) + train_len)]
+    valid.columns = ['close', 'Predictions']
+
+    valid['Predictions'] = y_pred
+
+    plt.figure(figsize=(100, 25))
+    plt.title('Model')
+    plt.xlabel('Time')
+    plt.ylabel('Price')
+    plt.plot(training['close'])
+    plt.plot(valid[['close', 'Predictions']])
+    plt.legend(['Train', 'Val', 'Predictions'])
+    plt.show()
+
+def main():
+
+    train_and_test_IPP()
+
+if __name__ == "__main__":
+    main()
